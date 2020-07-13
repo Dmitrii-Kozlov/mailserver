acf9df09e6fd2104310d990abab58739a4b1df06
LazyMan78 <demetra_n@front.ru>
2020-07-13 13:45:06 +0300

    создана отправка письма на сервер

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -16,6 +16,28 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
+  document.querySelector('#send_mail').addEventListener('click', () => {
+
+  const recipients = document.querySelector('#compose-recipients').value;
+  const subject =  document.querySelector('#compose-subject').value;
+  const body =  document.querySelector('#compose-body').value;
+
+      fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+          recipients: recipients,
+          subject: subject,
+          body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+       console.log(result);
+    });
+  })
+
+
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
@@ -28,14 +50,6 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
-      fetch('/emails/inbox')
-.then(response => response.json())
-.then(emails => {
-    // Print emails
-    console.log(emails);
-
-    // ... do something else with emails ...
-});
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;

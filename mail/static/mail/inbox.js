e96326ed47dbfb71f36f13f4a61e4e138ea3e516
LazyMan78 <demetra_n@front.ru>
2020-07-13 16:29:12 +0300

    создано отображение письма и добавлена кнопка unread на списке писем

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -10,6 +10,56 @@ document.addEventListener('DOMContentLoaded', function() {
   load_mailbox('inbox');
 });
 
+document.addEventListener('click', event => {
+                const element = event.target;
+                if (element.className === 'mail') {
+                   showMail(element.dataset.id)
+                } else if (element.className === 'unread'){
+                    markUnread(element.parentElement)
+                }
+                })
+
+function markUnread(element) {
+        fetch(`/emails/${element.dataset.id}`, {
+  method: 'PUT',
+  body: JSON.stringify({
+      read: false
+  })
+})
+element.style.backgroundColor = '#77dd11'
+}
+
+  function showMail(id) {
+    document.querySelector('#emails-view').innerHTML = ''
+      fetch(`/emails/${id}`)
+.then(response => response.json())
+.then(email => {
+    // Print email
+    console.log(email);
+
+    // ... do something else with email ...
+
+    const mail_sender = document.createElement('div');
+                mail_sender.innerHTML = `From: ${email.sender} `;
+    const mail_title = document.createElement('div');
+                mail_title.innerHTML = `Subject: ${email.subject} <br> <hr>`;
+    const mail_text = document.createElement('div');
+                mail_text.innerHTML = `${email.body}`;
+                // Add mail to DOM
+    document.querySelector('#emails-view').append(mail_sender);
+    document.querySelector('#emails-view').append(mail_title);
+    document.querySelector('#emails-view').append(mail_text);
+
+})
+      fetch(`/emails/${id}`, {
+  method: 'PUT',
+  body: JSON.stringify({
+      read: true
+  })
+})
+
+  }
+
 function compose_email() {
 
   // Show compose view and hide other views
@@ -54,7 +104,6 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-
   fetch(`/emails/${mailbox}`)
 .then(response => response.json())
 .then(emails => {
@@ -67,12 +116,17 @@ function load_mailbox(mailbox) {
   function addEmail(email) {
      const mail = document.createElement('div');
                 mail.className = 'mail';
+                mail.dataset.id = email.id
                 if (mailbox === 'inbox') {
-                    mail.innerHTML = `${email.sender} ${email.subject} ${email.timestamp}`;
+                    mail.innerHTML = `${email.sender} ${email.subject} ${email.timestamp} <button class="unread">Unread</button>`;
                 } else if (mailbox === 'sent'){
                     mail.innerHTML = `${email.recipients} ${email.subject} ${email.timestamp}`;
+                }
+                if (email.read) {
+                    mail.style.backgroundColor = 'gray'
                 }
                 // Add mail to DOM
                 document.querySelector('#emails-view').append(mail);
   }
+
 }
